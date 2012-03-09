03369adc930bd80a30014d0275f71a557e806f14
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-03-09 05:33:40 -0800

    Allow removal of a common prefix from template names. Refs #5

--- lib/jquery-tmpl-rails/jquery_tmpl_processor.rb
+++ lib/jquery-tmpl-rails/jquery_tmpl_processor.rb
@@ -8,6 +8,21 @@ module Sprockets
   class JqueryTmplProcessor < Tilt::Template
     include ActionView::Helpers::JavaScriptHelper
 
+    @template_prefix = ""
+
+    class << self
+      attr_reader :template_prefix
+
+      def template_prefix=(prefix)
+        if prefix.length > 0
+          prefix = prefix[1, prefix.length - 1] if prefix.start_with?("/")
+          prefix += "/" unless prefix.end_with?("/")
+        end
+
+        @template_prefix = prefix
+      end
+    end
+
     def self.default_mime_type
       'application/javascript'
     end
@@ -17,8 +32,14 @@ module Sprockets
 
     def evaluate(scope, locals, &block)
       <<-TMPL
-jQuery.template(#{scope.logical_path.inspect}, "#{escape_javascript data}");
+jQuery.template("#{template_name(scope)}", "#{escape_javascript data}");
         TMPL
+    end
+
+    private
+
+    def template_name(scope)
+      scope.logical_path.sub(self.class.template_prefix, "")
     end
   end
 
--- spec/jquery-tmpl-rails/jquery_tmpl_processor_spec.rb
+++ spec/jquery-tmpl-rails/jquery_tmpl_processor_spec.rb
@@ -1,15 +1,33 @@
 require "spec_helper"
 
 describe Sprockets::JqueryTmplProcessor do
-  let(:assets) do
-    assets = Sprockets::Environment.new
-    assets.append_path("spec/fixtures")
-    assets
+  before { described_class.template_prefix = "" }
+
+  let(:environment) do
+    environment = Sprockets::Environment.new
+    environment.append_path "."
+    environment
   end
 
   it "compiles templates with the .tmpl extension" do
-    assets["test"].to_s.should == <<-TMPL
-jQuery.template("test", "<div class=\\\"test\\\">{{test}}<\\/div>\\n");
+    asset = environment["spec/fixtures/test"]
+
+    asset.to_s.should == <<-TMPL
+jQuery.template("spec/fixtures/test", "<div class=\\\"test\\\">{{test}}<\\/div>\\n");
     TMPL
+  end
+
+  describe ".template_prefix" do
+    it "names templates with the logical path minus the template prefix" do
+      described_class.template_prefix = "spec/fixtures"
+      asset = environment["spec/fixtures/test"]
+      asset.to_s.should include('"test"')
+    end
+
+    it "normalizes template prefixes by removing extraneous slashes" do
+      described_class.template_prefix = "/spec/fixtures/"
+      asset = environment["spec/fixtures/test"]
+      asset.to_s.should include('"test"')
+    end
   end
 end
